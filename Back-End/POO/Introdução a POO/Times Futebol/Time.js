55790ca2aecc4d6739d41f51b774085fbc239443
Gabrielghsa <ghassis2018@gmail.com>
2026-09-24 16:03:02 -0300

    merge

---
+++ Back-End/POO/Introdução a POO/Times Futebol/Time.js
@@ -0,0 +1,12 @@
+class Time{
+    constructor(time1, time2){
+        this.time1 = time1;
+        this.time2 = time2;
+        this.jogadores = [];
+    }
+
+    adicionarJogadores(jogador){
+        
+    }
+
+}
